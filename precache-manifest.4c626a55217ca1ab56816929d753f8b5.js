2cce10696978d5ee9f49e316ae54373f557948e1
nyanchamaB <nyanchamabrendah3@gmail.com>
2024-08-04 20:45:39 +0300

    Updates

---
+++ precache-manifest.4c626a55217ca1ab56816929d753f8b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db9ccd9f43457a525ef350e965e2487",
+    "url": "/ifiweb/index.html"
+  },
+  {
+    "revision": "8d8dbb8181ae2636b44e",
+    "url": "/ifiweb/static/css/main.b04946af.chunk.css"
+  },
+  {
+    "revision": "c49c076c17b7f3dc5753",
+    "url": "/ifiweb/static/js/2.41bc1b16.chunk.js"
+  },
+  {
+    "revision": "178086a532b66218d956954447b4d1d1",
+    "url": "/ifiweb/static/js/2.41bc1b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37f5530f6a79c3f87cc",
+    "url": "/ifiweb/static/js/3.715c3464.chunk.js"
+  },
+  {
+    "revision": "8d8dbb8181ae2636b44e",
+    "url": "/ifiweb/static/js/main.9fd19f08.chunk.js"
+  },
+  {
+    "revision": "430483820030f3b71090",
+    "url": "/ifiweb/static/js/runtime-main.e74fdca1.js"
+  }
+]);
